290ed861dfea55c5c1dffc11f4e79f05ec27cb1f
Dewayne Allen <dewayneallen76@gmail.com>
2025-11-12 16:56:34 -0600

    Checking to see if Login is showing as expected on the portal

--- mtrx_portal_tests/login.py
+++ mtrx_portal_tests/login.py
@@ -5,11 +5,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
 driver = webdriver.Chrome()
+# Navigate to the MTRX Admin Portal
+driver.get("https://admin.mobiletechrx.com")
+# Find the element 
+element = driver.find_element(By.CLASS_NAME, "call-to-action")
+# Check that text is present
+assert element.text == "Log In"
 
-try:
-    driver.get("https://admin.mobiletechrx.com/login")
-
-    time.sleep(10)
-
-finally:
-    driver.quit()
+driver.quit()
